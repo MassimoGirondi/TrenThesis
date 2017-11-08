a4d34fd015d5e983f283c8d632da0ec378a8dd9a
MatteoBattilana <matteo@matteobattilana.com>
2017-11-08 13:10:35 +0100

    Setting up express for telegram-bot with node-telegram-bot-api library

--- bot/routes.js
+++ bot/routes.js
@@ -3,17 +3,30 @@
  * In example: the url called by Telegram
  */
 
-
+const TOKEN = '480161328:AAHT-yyzoOq5Qry1F45-WjqkTYcnP3xEkmg';
+const url = 'https://telegrambottrenthesis.herokuapp.com/bot';
+const TelegramBot = require('node-telegram-bot-api');
 var express = require('express');
+const bodyParser = require('body-parser');
 var router = express.Router();
 
+// No need to pass any parameters as we will handle the updates with Express
+const bot = new TelegramBot(TOKEN);
+// This informs the Telegram servers of the new webhook.
+bot.setWebHook(`${url}/bot${TOKEN}`);
 
 router
-  .get('/', function(req, res) {
-    res.json({
-      message: 'hooray! Benvenuto nelle nostre API per Telegram!'
-    });
-
-  })
+  .post(`/bot${TOKEN}`, function(req, res) {
+    bot.processUpdate(req.body);
+    res.status(200).send();
+  });
 
 module.exports = router;
+
+bot.on('message', msg => {
+    switch(msg.text.toString().toLowerCase()){
+        case "/start":
+            bot.sendMessage(msg.chat.id,"Seleziona una delle opzioni dalla tastiera in basso");
+        break;
+                                            }
+});
