61bc66b338ca0d658073579276ca19d6f8aa6f14
DanieleIsoni <daniele.isoni@studenti.unitn.it>
2017-11-15 14:57:20 +0100

    Added switch case for main button click, and added getJsonFromUrl and parseProfessors

--- bot/routes.js
+++ bot/routes.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var request = require('request');
+var fetch = require('node-fetch');
+const fetchJson = require('node-fetch-json');
 
 const TOKEN = process.env.tokenTelegramBot;
 const url = process.env.telegramWebHookUrl;
+const backButton = "Torna alle opzioni principali"
 const apiUrl = process.env.apiUrl;
 const TelegramBot = require('node-telegram-bot-api');
 const bodyParser = require('body-parser');
+const profEmoji = "\u{1f468}\u{200d}\u{1f3eb}"
 
 var router = express.Router();
 
@@ -14,71 +18,117 @@ const bot = new TelegramBot(TOKEN);
 // This informs the Telegram servers of the new webhook.
 bot.setWebHook(`${url}/bot${TOKEN}`);
 
-function getJsonFromUrl(url) {
-  var jsonRet = "";
-  request({url: url, json: true}, function(err, res, json) {
-    if (err) {
-      throw err;
-    }
-    //Return the json
-    jsonRet = json;
-    console.log("jsonRet: "+ jsonRet);
-  });
-  return jsonRet;
+function getJsonFromUrl(url, cb, chatId) {
+    request({
+        url: url,
+        json: true
+    }, function(err, res, json) {
+        if (err) {
+            throw err;
+        } else {
+            cb(json, chatId);
+        }
+    });
 }
 
-function parseProfessors(json){
-  console.log(json);
-  var jsonobj = JSON.parse(JSON.json);
+function parseProfessors(json, chatId) {
+    console.log(json);
+    var jsonobj = JSON.parse(JSON.stringify(json));
     //var tablename = arr[i].tablename;
-    console.log(jsonobj[0]["first_name"].toString());
+
+    var menu = [];
+    for (var i = 0; i < jsonobj.length; i++) {
+        menu.push(profEmoji + " " + jsonobj[i].first_name + " " + jsonobj[i].last_name);
+        
+    }
+    menu.push(backButton);
+
+    var options = {
+        reply_markup: JSON.stringify({
+            inline_keyboard: menu.map((x, xi) => ([{
+                text: x,
+                callback_data: xi,
+            }])),
+        }),
+    };
+
+    bot.sendMessage(chatId, "Seleziona il professore che più ti interessa e ti forniremo una lista delle tesi disponibili", options);
 }
+
+/*function fetchProfessors(requrl, chatId){
+    var json= fetchJson(requrl, {method:'GET'}).then(r => {return r}).catch(e => console.log(e));
+    console.log(json);
+}*/
 
 router
-  .post(`/bot${TOKEN}`, function(req, res) {
-    bot.processUpdate(req.body);
-    res.status(200).send();
-  });
+    .post(`/bot${TOKEN}`, function(req, res) {
+        bot.processUpdate(req.body);
+        res.status(200).send();
+    });
 
 module.exports = router;
 
 bot.on('message', msg => {
-    switch(msg.text.toString()){
+    switch (msg.text.toString()) {
         case "/start":
         case "Torna alle opzioni principali":
             bot.sendMessage(msg.chat.id, "Seleziona una delle opzioni dalla tastiera in basso", {
-            "reply_markup": {
-                "keyboard": [["Un argomento"], ["Il professore che preferisci"],   ["L'ambito di studi che preferisci"]]
+                "reply_markup": {
+                    "keyboard": [
+                        ["Un argomento"],
+                        ["Il professore che preferisci"],
+                        ["L'ambito di studi che preferisci"]
+                    ]
                 }
             });
-        break;
+            break;
         case "Un argomento":
             bot.sendMessage(msg.chat.id, "Seleziona uno dei principali argomenti e ti forniremo una lista delle testi disponibili", {
-            "reply_markup": {
-                "keyboard": [["Tesi1"], ["Tesi2"],   ["Tesi3"], ["Torna alle opzioni principali"]]
+                "reply_markup": {
+                    "keyboard": [
+                        ["Tesi1"],
+                        ["Tesi2"],
+                        ["Tesi3"],
+                        ["Torna alle opzioni principali"]
+                    ]
                 }
             });
-        break;
+            break;
 
         case "Il professore che preferisci":
-            var requrl = apiUrl+'/professors';
-            console.log(getJsonFromUrl(requrl));
-            bot.sendMessage(msg.chat.id, "Seleziona il professore che più ti interessa e ti forniremo una lista delle tesi disponibili", {
-            "reply_markup": {
-                "keyboard": [["Prof1"], ["Prof2"], ["Prof3"], ["Torna alle opzioni principali"]]
-                }
-            });
-        break;
+            var requrl = apiUrl + '/professors';
+            getJsonFromUrl(requrl, parseProfessors, msg.chat.id);
+            //fetchProfessors(requrl, msg.chat.id);
+
+            break;
 
         case "L'ambito di studi che preferisci":
             bot.sendMessage(msg.chat.id, "Seleziona l'ambito di studi che preferisci e ti forniremo una lista di testi disponibili", {
-            "reply_markup": {
-                "keyboard": [["Tesi1"], ["Tesi2"],   ["Tesi3"], ["Torna alle opzioni principali"]]
+                "reply_markup": {
+                    "keyboard": [
+                        ["Tesi1"],
+                        ["Tesi2"],
+                        ["Tesi3"],
+                        ["Torna alle opzioni principali"]
+                    ]
                 }
             });
-        break;
+            break;
         default:
-            bot.sendMessage(msg.chat.id, "Non ho capito");
-        break;
-                                            }
+            bot.sendMessage(msg.chat.id, "Non ho capito "+msg.text.toString());
+            if (msg.text.toString().length >= 5) {
+                switch (msg.text.toString().substring(0, 5)) {
+                    case profEmoji:
+                        bot.sendMessage(msg.chat.id, "Professore");
+                        break;
+                    default:
+                        bot.sendMessage(msg.chat.id, "Non ho capito");
+                        break;
+                }
+            } else {
+                bot.sendMessage(msg.chat.id, "Non ho capito");
+            }
+
+            break;
+    }
 });
