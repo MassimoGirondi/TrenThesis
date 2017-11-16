e37359f054ea1bb751b13fe3ad032041295b24c5
Matteo <matbat96@gmail.com>
2017-11-16 14:43:49 +0100

    Added callback for inline button

--- bot/routes.js
+++ bot/routes.js
@@ -17,6 +17,12 @@ var router = express.Router();
 const bot = new TelegramBot(TOKEN);
 // This informs the Telegram servers of the new webhook.
 bot.setWebHook(`${url}/bot${TOKEN}`);
+router
+    .post(`/bot${TOKEN}`, function(req, res) {
+        bot.processUpdate(req.body);
+        res.status(200).send();
+    });
+module.exports = router;
 
 function getJsonFromUrl(url, cb, chatId) {
     request({
@@ -31,23 +37,25 @@ function getJsonFromUrl(url, cb, chatId) {
     });
 }
 
+function parseJSON(url){
+  return fetchJSON(url, {method : 'GET'}).then(r => {return r.json})
+}
+
 function parseProfessors(json, chatId) {
     console.log(json);
     var jsonobj = JSON.parse(JSON.stringify(json));
-    //var tablename = arr[i].tablename;
 
-    var menu = [];
+    var professorName_id = [];
     for (var i = 0; i < jsonobj.length; i++) {
-        menu.push(profEmoji + " " + jsonobj[i].first_name + " " + jsonobj[i].last_name);
-        
+      //Pair for callback_data in inline button
+        professorName_id.push([profEmoji + " " + jsonobj[i].first_name + " " + jsonobj[i].last_name, jsonobj[i].id]);
     }
-    menu.push(backButton);
 
     var options = {
         reply_markup: JSON.stringify({
-            inline_keyboard: menu.map((x, xi) => ([{
-                text: x,
-                callback_data: xi,
+            inline_keyboard: professorName_id.map((x) => ([{
+                text: x[0],
+                callback_data: String("p"+x[1]),
             }])),
         }),
     };
@@ -55,19 +63,30 @@ function parseProfessors(json, chatId) {
     bot.sendMessage(chatId, "Seleziona il professore che più ti interessa e ti forniremo una lista delle tesi disponibili", options);
 }
 
-/*function fetchProfessors(requrl, chatId){
-    var json= fetchJson(requrl, {method:'GET'}).then(r => {return r}).catch(e => console.log(e));
-    console.log(json);
-}*/
 
-router
-    .post(`/bot${TOKEN}`, function(req, res) {
-        bot.processUpdate(req.body);
-        res.status(200).send();
-    });
 
-module.exports = router;
+// Inline button callback queries
+bot.on("callback_query", (callbackQuery) => {
+  //JSON parsing
+  const msg = callbackQuery.message;
+  var jsonobj = JSON.parse(JSON.stringify(callbackQuery));
 
+  console.log("ID numero"+jsonobj.data); // msg.data refers to the callback_data
+  switch (jsonobj.data.charAt(0)) {
+    case 'p':
+      //professore
+      bot.answerCallbackQuery(callbackQuery.id)
+        .then(() => bot.sendMessage(msg.chat.id, "You clicked!"));
+      break;
+    default:
+    bot.answerCallbackQuery(callbackQuery.id)
+          .then(() => bot.sendMessage(msg.chat.id, "Error!"));
+    break;
+
+  }
+});
+
+//Message
 bot.on('message', msg => {
     switch (msg.text.toString()) {
         case "/start":
@@ -97,7 +116,8 @@ bot.on('message', msg => {
 
         case "Il professore che preferisci":
             var requrl = apiUrl + '/professors';
-            getJsonFromUrl(requrl, parseProfessors, msg.chat.id);
+            console.log(parseJSON("http://echo.jsontest.com/key/value/one/two"));
+            //getJsonFromUrl(requrl, parseProfessors, msg.chat.id);
             //fetchProfessors(requrl, msg.chat.id);
 
             break;
