e4f678f74a0fc84d49f6387535b5aa1c07dcc08b
valodo <odorizzivalentina@gmail.com>
2017-11-19 19:30:56 +0100

    Add profile page

---
+++ website/js/tabs.js
@@ -0,0 +1,22 @@
+function openTab(name) {
+    var tabName = "#tab-" + name;
+    var containerName = "#container-" + name;
+    
+    $(".tab-selected").each(function(){
+        $(this).removeClass("tab-selected");
+    });
+    
+    $(".tab-container").each(function(){
+        $(this).hide();
+    });
+    
+    /*if(name == "profie"){
+        profile();
+    }
+    else if(name == "settings"){
+        settings();
+    }*/
+    
+    $(tabName).addClass("tab-selected");
+    $(containerName).show();
+}
