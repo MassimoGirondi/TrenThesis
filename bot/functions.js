639aa90afb675b37b110486dc1f223e407cf58af
Matteo <matbat96@gmail.com>
2017-11-20 18:45:41 +0100

    Moved function to module

--- bot/functions.js
+++ bot/functions.js
@@ -1,30 +1,126 @@
 /*
-* Main module that contains the base function, made to clean the bot logic
-* implementation in routers.js
-*/
-
+ * Main module that contains the base function, made to clean the bot logic
+ * implementation in routers.js
+ */
 /*
-* Required module for the Telegram bot
-*/
+ * Required module for the Telegram bot
+ */
 var request = require('request');
 
-exports.getJsonFromUrl = function(url, cb, chatId) {
+/*
+ * Const string
+ */
+const profEmoji = "\u{1f468}\u{200d}\u{1f3eb}";
+
+exports.getJsonFromUrl = function(url, cb, chatId, bot) {
     request({
         url: url,
         json: true
     }, function(err, res, json) {
         if (err) {
-            console.log("Json request error "+err);
+            console.log("Json request error " + err);
             throw err;
         } else {
             //must check json
             var jsonobj = JSON.parse(JSON.stringify(json));
-            if (jsonobj.hasOwnProperty('error')){
+            if (jsonobj.hasOwnProperty('error')) {
                 console.log("Json has error property");
                 throw "Wrong url";
-              }
-            else
-                cb(chatId, jsonobj);
+            } else
+                cb(chatId, jsonobj, bot);
         }
     });
+};
+
+
+exports.showProfessors = function(chatId, jsonobj, bot) {
+    var professorName_id = [];
+    for (var i = 0; i < jsonobj.length; i++) {
+        //Pair for callback_data in inline button
+        //Check also if jsonobj has the right properties
+        if (jsonobj[i].hasOwnProperty('first_name') && jsonobj[i].hasOwnProperty('last_name') && jsonobj[i].hasOwnProperty('id'))
+            professorName_id.push([profEmoji + " " + jsonobj[i].first_name + " " + jsonobj[i].last_name, jsonobj[i].id]);
+    }
+
+    var options = {
+        reply_markup: JSON.stringify({
+            inline_keyboard: professorName_id.map((x) => ([{
+                text: x[0],
+                callback_data: String("p" + x[1]),
+            }])),
+        }),
+    };
+
+    bot.sendMessage(chatId, jsonobj.length > 0 ? "Seleziona il professore che più ti interessa e ti forniremo una lista delle tesi disponibili" : "Non ci sono professori", options);
+};
+
+exports.showAllThesis = function(chatId, jsonobj, bot) {
+    var thesisName_id = [];
+    for (var i = 0; i < jsonobj.length; i++) {
+        if (jsonobj[i].hasOwnProperty('title'))
+            thesisName_id.push([jsonobj[i].title, jsonobj[i].id]);
+    }
+
+    var options = {
+        reply_markup: JSON.stringify({
+            inline_keyboard: thesisName_id.map((x) => ([{
+                text: x[0],
+                callback_data: String("t" + x[1]),
+            }])),
+        }),
+    };
+
+    bot.sendMessage(chatId, jsonobj.length > 0 ? "Seleziona la tesi che più ti interessa e ti forniremo i suoi dati" : "Non ci sono tesi", options);
+};
+
+exports.showProfessor_CategoryThesis = function(chatId, jsonobj, bot) {
+
+    var thesisName_id = [];
+    for (var i = 0; i < jsonobj.length; i++) {
+        if (jsonobj[i].hasOwnProperty('title'))
+            thesisName_id.push([jsonobj[i].title, jsonobj[i].id]);
+    }
+
+    var options = {
+        reply_markup: JSON.stringify({
+            inline_keyboard: thesisName_id.map((x) => ([{
+                text: x[0],
+                callback_data: String("t" + x[1]),
+            }])),
+        }),
+    };
+
+    bot.sendMessage(chatId, jsonobj.length > 0 ? "Seleziona la tesi che più ti interessa e ti forniremo i suoi dati" : "Non ci sono tesi", options);
+};
+
+exports.showCategories = function(chatId, jsonobj, bot) {
+
+    var categoriesName_id = [];
+    for (var i = 0; i < jsonobj.length; i++) {
+        //Pair for callback_data in inline button
+        categoriesName_id.push([jsonobj[i], jsonobj[i]]);
+    }
+
+    var options = {
+        reply_markup: JSON.stringify({
+            inline_keyboard: categoriesName_id.map((x) => ([{
+                text: x[0],
+                callback_data: String("c" + x[1]),
+            }])),
+        }),
+    };
+
+    bot.sendMessage(chatId, jsonobj.length > 0 ? "Seleziona la categoria che più ti interessa e ti forniremo una lista delle tesi disponibili" : "Non ci sono categorie", options);
+};
+
+
+
+exports.showThesisInfo = function(chatId, jsonobj, bot) {
+
+    var thesis = "Titolo:\t" + jsonobj.title +
+        "\nAnteprima:\t" + jsonobj.short_abstract +
+        "\nDescrizione:\t" + jsonobj.description +
+        "\nUrl:\t" + jsonobj.resource;
+
+    bot.sendMessage(chatId, thesis);
 };
--- bot/routes.js
+++ bot/routes.js
@@ -1,16 +1,12 @@
 /*
-* Required module for the Telegram bot
-*/
+ * Required module for the Telegram bot
+ */
 var express = require('express');
-var fetch = require('node-fetch');
-const fetchJson = require('node-fetch-json');
-const TelegramBot = require('node-telegram-bot-api');
-const bodyParser = require('body-parser');
 var functions = require('./functions.js');
 
 /*
-* Const get from process.env (Heroku env variable)
-*/
+ * Const get from process.env (Heroku env variable)
+ */
 //Used to keep the telegram token secret
 const TOKEN = process.env.tokenTelegramBot;
 //WebHook telegram, made to allow the possibility to migrate to a new host
@@ -19,15 +15,15 @@ const url = process.env.telegramWebHookUrl;
 const apiUrl = process.env.apiUrl;
 
 /*
-* Const string
-*/
+ * Const string
+ */
 const backButton = "Torna alle opzioni principali"
-const profEmoji = "\u{1f468}\u{200d}\u{1f3eb}"
+const TelegramBot = require('node-telegram-bot-api');
 
 /*
-* Telegram integrated with express
-* Initialization section
-*/
+ * Telegram integrated with express
+ * Initialization section
+ */
 var router = express.Router();
 // No need to pass any parameters as we will handle the updates with Express
 const bot = new TelegramBot(TOKEN);
@@ -40,98 +36,6 @@ router
     });
 module.exports = router;
 
-
-function parseProfessors(chatId, jsonobj) {
-    var professorName_id = [];
-    for (var i = 0; i < jsonobj.length; i++) {
-        //Pair for callback_data in inline button
-        //Check also if jsonobj has the right properties
-        if (jsonobj[i].hasOwnProperty('first_name') && jsonobj[i].hasOwnProperty('last_name') && jsonobj[i].hasOwnProperty('id'))
-            professorName_id.push([profEmoji + " " + jsonobj[i].first_name + " " + jsonobj[i].last_name, jsonobj[i].id]);
-    }
-
-    var options = {
-        reply_markup: JSON.stringify({
-            inline_keyboard: professorName_id.map((x) => ([{
-                text: x[0],
-                callback_data: String("p" + x[1]),
-            }])),
-        }),
-    };
-
-    bot.sendMessage(chatId, jsonobj.length > 0 ? "Seleziona il professore che più ti interessa e ti forniremo una lista delle tesi disponibili" : "Non ci sono professori", options);
-}
-
-function showAllThesis(chatId, jsonobj) {
-    var thesisName_id = [];
-    for (var i = 0; i < jsonobj.length; i++) {
-        if (jsonobj[i].hasOwnProperty('title'))
-            thesisName_id.push([jsonobj[i].title, jsonobj[i].id]);
-    }
-
-    var options = {
-        reply_markup: JSON.stringify({
-            inline_keyboard: thesisName_id.map((x) => ([{
-                text: x[0],
-                callback_data: String("t" + x[1]),
-            }])),
-        }),
-    };
-
-    bot.sendMessage(chatId, jsonobj.length > 0 ? "Seleziona la tesi che più ti interessa e ti forniremo i suoi dati" : "Non ci sono tesi", options);
-}
-
-function showProfessor_CategoryThesis(chatId, jsonobj) {
-
-    var thesisName_id = [];
-    for (var i = 0; i < jsonobj.length; i++) {
-        //if (jsonobj[i].hasOwnProperty('title'))
-            thesisName_id.push([jsonobj[i].title, jsonobj[i].id]);
-    }
-
-    var options = {
-        reply_markup: JSON.stringify({
-            inline_keyboard: thesisName_id.map((x) => ([{
-                text: x[0],
-                callback_data: String("t" + x[1]),
-            }])),
-        }),
-    };
-
-    bot.sendMessage(chatId, jsonobj.length > 0 ? "Seleziona la tesi che più ti interessa e ti forniremo i suoi dati" : "Non ci sono tesi", options);
-}
-
-function parseCategories(chatId, jsonobj) {
-
-    var categoriesName_id = [];
-    for (var i = 0; i < jsonobj.length; i++) {
-        //Pair for callback_data in inline button
-        categoriesName_id.push([jsonobj[i], jsonobj[i]]);
-    }
-
-    var options = {
-        reply_markup: JSON.stringify({
-            inline_keyboard: categoriesName_id.map((x) => ([{
-                text: x[0],
-                callback_data: String("c" + x[1]),
-            }])),
-        }),
-    };
-
-    bot.sendMessage(chatId, jsonobj.length > 0 ? "Seleziona la categoria che più ti interessa e ti forniremo una lista delle tesi disponibili" : "Non ci sono categorie", options);
-}
-
-
-
-function showThesisInfo(chatId, jsonobj) {
-
-    var thesis = "Titolo:\t" + jsonobj.title +
-        "\nAnteprima:\t" + jsonobj.short_abstract +
-        "\nDescrizione:\t" + jsonobj.description +
-        "\nUrl:\t" + jsonobj.resource;
-
-    bot.sendMessage(chatId, thesis);
-}
 
 // Inline button callback queries
 bot.on("callback_query", (callbackQuery) => {
@@ -146,19 +50,19 @@ bot.on("callback_query", (callbackQuery) => {
             requrl = String(apiUrl + "/topics?professor_id=" + jsonobj.data.substring(1));
             //professore
             bot.answerCallbackQuery(callbackQuery.id)
-                .then(() => functions.getJsonFromUrl(requrl, showProfessor_CategoryThesis, msg.chat.id));
+                .then(() => functions.getJsonFromUrl(requrl, functions.showProfessor_CategoryThesis, msg.chat.id, bot));
             break;
         case 't':
             requrl = String(apiUrl + "/topics/" + jsonobj.data.substring(1));
             //clicked on topic
             bot.answerCallbackQuery(callbackQuery.id)
-                .then(() => functions.getJsonFromUrl(requrl, showThesisInfo, msg.chat.id));
+                .then(() => functions.getJsonFromUrl(requrl, functions.showThesisInfo, msg.chat.id, bot));
             break;
         case 'c':
             requrl = String(apiUrl + "/topics?category=" + jsonobj.data.substring(1));
             //clicked on topic
             bot.answerCallbackQuery(callbackQuery.id)
-                .then(() => functions.getJsonFromUrl(requrl, showProfessor_CategoryThesis, msg.chat.id));
+                .then(() => functions.getJsonFromUrl(requrl, functions.showProfessor_CategoryThesis, msg.chat.id, bot));
             break;
         default:
             bot.answerCallbackQuery(callbackQuery.id)
@@ -185,19 +89,17 @@ bot.on('message', msg => {
             break;
         case "Il professore che preferisci":
             var requrl = apiUrl + "/professors";
-            functions.getJsonFromUrl(requrl, parseProfessors, msg.chat.id);
+            functions.getJsonFromUrl(requrl, functions.showProfessors, msg.chat.id, bot);
 
             break;
         case "Un argomento":
             var requrl = apiUrl + "/topics";
-            functions.getJsonFromUrl(requrl, showAllThesis, msg.chat.id);
+            functions.getJsonFromUrl(requrl, functions.showAllThesis, msg.chat.id, bot);
             break;
-
-
 
         case "L'ambito di studi che preferisci":
             var requrl = apiUrl + "/categories";
-            functions.getJsonFromUrl(requrl, parseCategories, msg.chat.id);
+            functions.getJsonFromUrl(requrl, functions.showCategories, msg.chat.id, bot);
             break;
         default:
             bot.sendMessage(msg.chat.id, "Non ho capito");
