5150a77f2ad978529d51c43bbca430c1b4ef8c25
Riccardo Capraro <capraroriccardo@gmail.com>
2017-11-25 11:54:09 +0100

    Implement delete topic in idea_page

--- docs/website/js/idea_page.js
+++ docs/website/js/idea_page.js
@@ -14,5 +14,18 @@ function contentIdea() {
 }
 
 function remove() {
-
+  $.ajax({
+    url: api_url + '/api/topics/' + Cookies.getJSON('topic').id,
+    type: 'DELETE',
+    data: {
+      'professor_id': Cookies.getJSON('profile').id,
+      'token': Cookies.get('token')
+    },
+    success: function() {
+      window.location.href = 'idee.html';
+    },
+    error: (err) => {
+      console.log('Failed to delete the topic with error: ' + err);
+    }
+  });
 }
