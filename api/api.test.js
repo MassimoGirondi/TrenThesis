f5338f9253fb0cd0c67733d36f6e56207f3f81c7
Massimo Girondi <massimo@girondi.net>
2017-11-29 16:09:37 +0100

    Add Import DB before tests

--- api/api.test.js
+++ api/api.test.js
@@ -1,14 +1,65 @@
 const request = require('supertest');
 const app = require('../router');
 const getTestToken = require('./utils').getTestToken;
-/*Wait the DB connection, then do the tests*/
-beforeAll((done) => {
-  app.DBConnect(() => {
-    done()
-  })
+const exec = require('child_process').exec;
 
+/*
+  Function to call mongoimport
+*/
+function importTable(name, cb) {
+  var options = '--host localhost --port 27017 --db trenthesis';
+  options += ' --collection ' + name;
+  options += ' --drop --maintainInsertionOrder';
+  options += ' --file tools/test_populations/' + name + '.json';
+
+  exec('mongoimport ' + options, {
+    cwd: '.'
+  }, (err, stdout, stderr) => {
+    //console.log("Imported " + name); // + ": " + stderr);
+    cb();
+  })
+}
+
+function importAll(cb) {
+
+  //Set DB to local instance
+  process.env.mongoDBUrl = 'mongodb://localhost:27017/trenthesis';
+  //Import the DB
+  importTable('users', () => {
+    importTable('categories', () => {
+      importTable('professors', () => {
+        importTable('topics', () => {
+          //console.log("Test Database Loaded!");
+
+          //Connect to DB
+          app.DBConnect(() => {
+            cb()
+          })
+        });
+      });
+    });
+  });
+}
+
+
+
+/*Wait the DB population and connection, then do the tests*/
+beforeAll(importAll);
+
+/*Restore the DB*/
+afterAll((done) => {
+  app.get('db').close(() => {
+    importAll(() => {
+      done();
+    });
+  });
 })
 
+
+
+describe('Test Get professors', async () => {
+  /*put tests here*/
+});
 
 
 describe('Test Professor Update', () => {
@@ -23,15 +74,11 @@ describe('Test Professor Update', () => {
       .set('x-access-token', getTestToken())
       .then(response => {
         expect(response.statusCode).toBe(200)
-        return app.get('db').collection('professors').findOne({
-          'id': 1
-        })
-
-      })
-      .then(professor => {
-        expect(professor.first_name).toBe('Guido');
-        expect(professor.last_name).toBe('La Barca')
-
+        return request(app)
+          .get('/api/professors/1')
+      }).then((response) => {
+        expect(response.body.first_name).toEqual('Guido')
+        expect(response.body.last_name).toEqual('La Barca')
       })
   })
 
@@ -46,8 +93,6 @@ describe('Test Professor Update', () => {
       .set('x-access-token', getTestToken())
       .then(response => {
         expect(response.statusCode).toBe(403)
-
       })
-
   })
 })
